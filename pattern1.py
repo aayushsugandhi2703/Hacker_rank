9cb1b06ba2ed3d31243d25d9f72b3a86bce6ab95
Aayush Sugandhi <aayush.sugandhi@gmail.com>
2025-02-10 20:05:00 +0530

    added another program for pattern

---
+++ pattern1.py
@@ -0,0 +1,13 @@
+n = int(input())  
+
+for i in range(1, n):  # Loop from 1 to n-1
+    print(str(i) * i)  # Print i repeated i times
+
+
+# another way to do this is:
+
+for i in range(1,n):
+    sum =0
+    for j in range(i):
+        sum =(sum *10) +i
+    print(sum)

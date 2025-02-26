2f7eb9abb28df20d9d407e4dc01a728a094bc0b3
Aayush Sugandhi <aayush.sugandhi@gmail.com>
2025-02-26 07:11:55 +0530

    addded another problem

--- iter_combo.py
+++ iter_combo.py
@@ -1,3 +1,6 @@
+# this program defines a function that takes a string and an integer as input
+# and returns a list of all possible combinations of the string of length n
+# the function uses the itertools module to generate the combinations
 from itertools import combinations
 
 s,n = input().split()

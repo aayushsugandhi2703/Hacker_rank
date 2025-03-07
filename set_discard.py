84407ed808262b5363f64490fff64c2f7eff7883
Aayush Sugandhi <aayush.sugandhi@gmail.com>
2025-03-07 06:24:41 +0530

    changed hte logic

--- set_discard.py
+++ set_discard.py
@@ -6,10 +6,12 @@ commands = [input().split() for _ in range(op)]
 
 for cmd in commands:
     if cmd[0] == 'pop':
-        nums.pop() 
+        if nums:  # Check if set is non-empty before popping
+            nums.remove(min(nums))
     elif cmd[0] == 'remove':
-        nums.remove(int(cmd[1]))  
+        if int(cmd[1]) in nums:  # Check before removing to avoid KeyError
+            nums.remove(int(cmd[1]))  
     elif cmd[0] == 'discard':
-        nums.discard(int(cmd[1]))  
+        nums.discard(int(cmd[1]))  # Safe, no KeyError raised
 
 print(sum(nums))  

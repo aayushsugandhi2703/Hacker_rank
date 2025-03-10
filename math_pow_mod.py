322c6e87f153a803ceb56d4511119a271bf425b3
Aayush Sugandhi <aayush.sugandhi@gmail.com>
2025-03-10 20:51:26 +0530

    solved another problem for math library

---
+++ math_pow_mod.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+m = int(input())
+
+# it is used to calculate a^b
+# pow(a,b) is equal to a**b
+print(pow(a,b))
+
+# it is used to calculate a^b % m
+# pow(a,b,m) is equal to a**b modulo m
+print(pow(a,b,m))

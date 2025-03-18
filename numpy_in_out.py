77fd31bc2697f7e71fe0f4f52c9d3f77d4d3a313
Aayush Sugandhi <aayush.sugandhi@gmail.com>
2025-03-18 07:41:43 +0530

    added another problem

--- numpy_in_out.py
+++ numpy_in_out.py
@@ -1,3 +1,8 @@
+# This is a solution to the problem of numpy inner and outer problem in the domain of python on the platform of HackerRank.
+# Problem Statement: You are given two arrays: A and B. Your task is to compute their inner and outer product.
+# Inner Product: The inner product of two arrays is an array of the same dimension, which is calculated by multiplying the elements of the arrays.
+# Outer Product: The outer product of two arrays is an array of the shape (A.shape[0], B.shape[0]), where the element 'i' of the array is the product of the elements of the 'i'th element of the first array and all the elements of the second array.
+
 import numpy
 arr1 = numpy.array(input().split() , int)
 arr2 = numpy.array(input().split() , int)

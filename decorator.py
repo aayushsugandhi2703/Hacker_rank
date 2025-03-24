6b8037b9baf5df877cecda0ec0af3ddd5f1ad47e
Aayush Sugandhi <aayush.sugandhi@gmail.com>
2025-03-24 07:52:39 +0530

    solved another problem

---
+++ decorator.py
@@ -0,0 +1,18 @@
+def wrapper(f):
+    def fun(l):
+        arr =[]
+        for i in l:
+            num = i[-10:]  # Extract last 10 digits
+            formatted_num = f"+91 {num[:5]} {num[5:]}"
+            arr.append(formatted_num)
+        # Call the original function with the formatted list
+        return f(arr)
+    return fun
+
+@wrapper
+def sort_phone(l):
+    print(*sorted(l), sep='\n')
+
+if __name__ == '__main__':
+    l = [input() for _ in range(int(input()))]
+    sort_phone(l) 

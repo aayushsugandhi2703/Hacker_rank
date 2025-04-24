a9da51fc8575b3d9de9ef990cd79d3157181a106
Aayush Sugandhi <aayush.sugandhi@gmail.com>
2025-04-24 05:47:15 +0530

    addded new problem

---
+++ Data-Structures/count-fruits.py
@@ -0,0 +1,61 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countApplesAndOranges' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER s
+#  2. INTEGER t
+#  3. INTEGER a
+#  4. INTEGER b
+#  5. INTEGER_ARRAY apples
+#  6. INTEGER_ARRAY oranges
+#
+
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    counta = 0
+    counto = 0
+
+    for d in apples:
+        landing_point = a + d
+        if landing_point >= s and landing_point <= t:
+            counta += 1
+
+    for d in oranges:
+        landing_point = b + d
+        if landing_point >= s and landing_point <= t:
+            counto += 1
+
+    print(counta)
+    print(counto)
+    
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    s = int(first_multiple_input[0])
+
+    t = int(first_multiple_input[1])
+
+    second_multiple_input = input().rstrip().split()
+
+    a = int(second_multiple_input[0])
+
+    b = int(second_multiple_input[1])
+
+    third_multiple_input = input().rstrip().split()
+
+    m = int(third_multiple_input[0])
+
+    n = int(third_multiple_input[1])
+
+    apples = list(map(int, input().rstrip().split()))
+
+    oranges = list(map(int, input().rstrip().split()))
+
+    countApplesAndOranges(s, t, a, b, apples, oranges)

94cbfd4dfbac08d883703484020637d8b4682eb3
Aayush Sugandhi <aayush.sugandhi@gmail.com>
2025-05-01 06:52:42 +0530

    added new problem

---
+++ Data-Structures/count-divide-posiblity.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'findDigits' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def findDigits(n):
+    digits = [int(d) for d in str(n)]
+    count = 0
+    for i in digits:
+        if i == 0:
+            continue  # skip zero digits
+        if n % i == 0:
+            count += 1
+    return count
+
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = findDigits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
